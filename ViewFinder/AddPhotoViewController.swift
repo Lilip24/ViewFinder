7cbd6d21d7bbf31c51617d5f394f088dc7debc89
Lili <educationapplication2024@gmail.com>
2019-07-30 10:47:58 -0400

    End of Iteration 1

--- ViewFinder/AddPhotoViewController.swift
+++ ViewFinder/AddPhotoViewController.swift
@@ -8,15 +8,26 @@
 
 import UIKit
 
-class AddPhotoViewController: UIViewController {
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
-    
-
+    @IBAction func takeSelfieTapped(_sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+     internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{}
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+}
     /*
     // MARK: - Navigation
 
@@ -27,4 +38,4 @@ class AddPhotoViewController: UIViewController {
     }
     */
 
-}
+
